95b38ad65137ff8dbbbfaa67728de91f18bed814
gabrielavaliente <00010122@uca.edu.sv>
2025-04-01 22:22:47 -0600

    Agregando Clase Paciente

--- Lab01Hospital/src/hospital/model/Paciente.java
+++ Lab01Hospital/src/hospital/model/Paciente.java
@@ -1,4 +1,26 @@
 package hospital.model;
 
 public class Paciente {
+    private String nombre;
+    private String apellido;
+    private String dui;
+    private String cumpleanos;
+
+    public Paciente(String nombre, String apellido, String dui, String cumpleanos) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dui = dui;
+        this.cumpleanos = cumpleanos;
+    }
+
+    // Getters y Setters
+    public String getNombre() { return nombre; }
+    public String getApellido() { return apellido; }
+    public String getDui() { return dui; }
+    public String getCumpleanos() { return cumpleanos; }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido + " (DUI: " + dui + ")";
+    }
 }
